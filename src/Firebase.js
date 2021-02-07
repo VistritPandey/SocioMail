d1444ff381aa2c9c6ec37446fa167f557f5e7a53
vistritpandey <vistritpandey@gmail.com>
2021-02-07 11:38:15 +0530

    k

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,7 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  // add your own config file
+  apiKey: "AIzaSyDgpb2a9MP95TgjOK8SEbKtTu6cNn0BR4U",
+  authDomain: "socio-email.firebaseapp.com",
+  projectId: "socio-email",
+  storageBucket: "socio-email.appspot.com",
+  messagingSenderId: "190989870950",
+  appId: "1:190989870950:web:140f1f072981d97b590aa7",
+  measurementId: "G-J7XE31T75P",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
